702c932658b1a716c49724bbabef1cf6dfea2d4d
DKAce330 <139510747+DKAce330@users.noreply.github.com>
2023-12-04 17:07:03 -0800

    Working Start and Installer .bat files

--- Installer.bat
+++ Installer.bat
@@ -1,3 +1,10 @@
 @echo off
-
+::Create a virtual environment
+virtualenv scrub~
+::Activate virtual environment
+source scrub~/bin/activate
+::Install libraries
+pip install splinter
+pip install bs4
+::Delete the installer
 del "%~f0"
--- Start.bat
+++ Start.bat
@@ -1,3 +1,6 @@
 @echo off
+::Activate virtual environment
+source scrub~/bin/activate
+::Run script
 cmd /k python Scrubber.py
 if %errorlevel% neq 0 echo An error occurred while running the script.
